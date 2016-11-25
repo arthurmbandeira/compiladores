310505e872115e9f82787d4b3376b31912a86c5f
Arthur <arthurmbandeira@gmail.com>
2016-11-25 03:27:31 -0200

    Leitura e Gramatica ok.

--- first-follow/first-follow.c
+++ first-follow/first-follow.c
@@ -8,13 +8,28 @@
 #define INICIAL 'S'
 #define FINAL '$'
 
-int isTerminal(char t){
-    return !isupper(t);
+int numRules;
+char *firstSet;
+
+// Structs
+struct SRules{
+    char head;
+    char *production;
+};
+typedef struct SRules Rules;
+
+Rules *newRules(n){
+    Rules *rules = malloc(n*sizeof(Rules));
+    rules->head = ' ';
+    rules->production = malloc(sizeof(Rules));
+    return rules;
 }
 
+// File & Grammar Related
 void readFile(char *out, char *path){
     char c;
     char *aux = malloc(sizeof(char));
+
     FILE *file;
 
     file = fopen(path, "r");
@@ -31,14 +46,72 @@ void readFile(char *out, char *path){
         }
     }
 
-    free(aux);
     fclose(file);
+    free(aux);
+}
+
+void treatGrammar(char *in, Rules *rule){
+    int i;
+
+    char head;
+    char *line = NULL;
+    char *prod = NULL;
+
+    while((line = strsep(&in, "\n")) != NULL){
+        head = line[0];
+        line = &line[2];
+
+        while((prod = strsep(&line, "|")) != NULL){            
+            rule[numRules].head = head;
+            rule[numRules].production = prod;
+            numRules++;
+        }
+    }
+
+    free(line);
+    free(prod);
+}
+
+void printGrammar(Rules *grammar){
+    int i;
+
+    for (i = 0; i < numRules; i++){
+        printf("%c -> %s\n", grammar[i].head, grammar[i].production);
+    }
+}
+
+int isTerminal(char t){
+    return !isupper(t);
+}
+
+void append(char *firstSet, char c){}
+
+char *getFirstSet(char c){}
+
+void first(char c){
+    int i;
+    if (isTerminal(c)){
+        append(firstSet, c);
+    } else {
+        for (i = 0; i < strlen(getFirstSet(c)); i++){
+            printf("0k\n");
+        }
+    }
 }
 
 int main(int argc, char *argv[]){
+    int i;
+
     char *in = malloc(sizeof(char));
     readFile(in, argv[ARQUIVO]);
-    printf("%s\n", in);
+
+    Rules *rules = newRules(numRules);
+
+    treatGrammar(in, rules);
+    printGrammar(rules);
+    
+    free(in);
+    /*free(rules);*/
 
     return 0;
 }
