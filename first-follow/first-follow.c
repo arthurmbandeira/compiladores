8afac2a6da31e494f94a1bfe6249d75eee83a2a9
Arthur <arthurmbandeira@gmail.com>
2016-11-25 20:56:52 -0200

    Grandes Avanços.

--- first-follow/first-follow.c
+++ first-follow/first-follow.c
@@ -10,17 +10,19 @@
 #define INICIAL 'S'
 #define FINAL '$'
 
-int numRules, numChars;
-char *firstSet;
+int numRules, numChars, numFirstSets;
+
+char *ravelSet;
 
 // Structs
 struct SRules{
     char head;
-    char *production;
+    char *rightSide;
 };
 typedef struct SRules Rules;
 
 Rules *rules;
+Rules *firstSet;
 
 void newRules(){
     int i;
@@ -28,10 +30,11 @@ void newRules(){
 
     for (i = 0; i < GRSIZE; ++i){
         rules->head = 0;
-        rules->production = NULL;
+        rules->rightSide = NULL;
     }
 }
 
+// File & Grammar Related
 void treatGrammar(char *in){
 
     char head;
@@ -44,10 +47,10 @@ void treatGrammar(char *in){
 
         while((prod = strsep(&line, "|")) != NULL){            
             rules[numRules].head = head;
-            rules[numRules].production = malloc(100 * sizeof(char));
-            memset(rules[numRules].production, '\0', 100);
+            rules[numRules].rightSide = malloc(100 * sizeof(char));
+            memset(rules[numRules].rightSide, '\0', 100);
 
-            strcpy(rules[numRules].production, prod);
+            strcpy(rules[numRules].rightSide, prod);
             numRules++;
         }
     }
@@ -57,15 +60,13 @@ void printGrammar(){
     int i;
 
     for (i = 0; i < numRules; i++){
-        printf("%c -> %s\n", rules[i].head, rules[i].production);
+        printf("%c -> %s\n", rules[i].head, rules[i].rightSide);
     }
 }
 
-// File & Grammar Related
 void readFile(char *path){
     char c, head;
 
-    //char *buffer = (char *)malloc(1024*sizeof(char));
     char aux[2];
     char buffer[1024];
 
@@ -98,18 +99,91 @@ int isTerminal(char t){
     return !isupper(t);
 }
 
-void append(char *firstSet, char c){}
+int hasLambda(int prod){
+    if (strcmp(rules[prod].rightSide, "e") == 0)
+        return 1;
+    return 0;
+}
 
 char *getFirstSet(char c){}
 
-void first(char c){
+void append(char *set, char token){
     int i;
-    if (isTerminal(c)){
-        append(firstSet, c);
-    } else {
-        for (i = 0; i < strlen(getFirstSet(c)); i++){
-            printf("0k\n");
+
+    char aux[2];
+    memset(aux, '\0', sizeof(aux));
+
+    for (i = 0; i < strlen(set); i++){
+        if (set[i] == token) return;
+    }
+    snprintf(aux, sizeof(aux), "%c", token);
+    strncat(set, aux, strlen(aux));
+}
+
+void unionSet(){}
+
+void createFirst(char new){
+    firstSet[numFirstSets].head = new;
+    firstSet[numFirstSets].rightSide = malloc(GRSIZE * sizeof(char));
+    memset(firstSet[numFirstSets].rightSide, '\0', GRSIZE);
+    if (isTerminal(new)){
+        append(firstSet[numFirstSets].rightSide, new);
+    }
+    numFirstSets++;
+}
+
+void ravelTerminals(char *right){
+    int i;
+    for (i = 0; i < strlen(right); i++){
+        if (isTerminal(right[i])){
+            append(ravelSet, right[i]);
         }
+    }
+}
+
+void ravelNotTerminals(char left){
+    append(ravelSet, left);
+}
+
+void createRavelSet(){
+    int i;
+    ravelSet = malloc(GRSIZE * sizeof(char));
+    memset(ravelSet, '\0', GRSIZE);
+    for (i = 0; i < numRules; i++){
+        ravelTerminals(rules[i].rightSide);
+        ravelNotTerminals(rules[i].head);
+    }
+}
+
+void createFirstSets(){
+    int i, j;
+    int exists;
+    firstSet = malloc(GRSIZE * sizeof(Rules));
+    for (i = 0; i < strlen(ravelSet); i++){
+        exists = 0;
+        for (j = 0; j < numFirstSets; j++){
+            if (firstSet[numFirstSets].head == ravelSet[i]){
+                exists = 1;
+                break;
+            }
+        }
+        if (!exists) createFirst(ravelSet[i]);
+    }
+}
+
+void addToFirst(int ruleIndex, char token){
+    int i;
+    for (i = 0; i < numFirstSets; i++){
+        if (firstSet[i].head == rules[ruleIndex].head){
+            append(firstSet[i].rightSide, token);
+        }
+        
+    }
+}
+
+void first(int ruleIndex){
+    if (hasLambda(ruleIndex)){
+        addToFirst(ruleIndex, 'e');
     }
 }
 
@@ -117,9 +191,27 @@ int main(int argc, char *argv[]){
     int i;    
 
     newRules();
+
     readFile(argv[ARQUIVO]);
 
-    printGrammar();
+    createRavelSet();
+    printf("ravel set: %s\n", ravelSet);
+    createFirstSets();
+
+    
+    /*printGrammar();*/
+
+    for (i = 0; i < numRules; i++){
+        first(i);
+
+    }
+    /*addToFirst(0, 'k');*/
+
+    for (i = 0; i < numFirstSets; i++){
+        printf("first sets: %c -> %s\n", firstSet[i].head, firstSet[i].rightSide);
+    }
+
+    
 
     /*treatGrammar(in, rules);
     printGrammar(rules);*/
